ca974b1a53c9a340c2f93861b30801b9a02b131b
thomas-rooty <tc.caron91@gmail.com>
2023-02-03 14:20:25 +0100

    added cv to download on Footer.tsx

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -9,6 +9,7 @@ const Footer = () => {
         <FooterButton href="https://github.com/thomas-rooty" imgSrc={"/assets/github.svg"} imgAlt="Github"/>
         <FooterButton href="https://www.linkedin.com/in/dev-thomas-caron" imgSrc={"/assets/linkedin.svg"} imgAlt="LinkedIn"/>
         <FooterButton href={"https://www.instagram.com/tho_macaron"} imgSrc={"/assets/instagram.svg"} imgAlt="Instagram"/>
+        <FooterButton href={"https://github.com/thomas-rooty/thomas-caron/raw/main/public/assets/CV%20Thomas%20CARON%20-%202023.pdf"} imgSrc={"/assets/download.svg"} imgAlt="CV"/>
       </div>
     </footer>
   )
